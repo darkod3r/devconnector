4bdc0d605875633350797c0e49bd236b8545b5ae
darkod3r <ankitpra15@gmail.com>
2019-02-15 16:49:28 +0530

    passport auth done

--- server.js
+++ server.js
@@ -3,12 +3,13 @@ const app = express();
 const mongoose = require(`mongoose`);
 const port = process.env.PORT || 5000;
 const db = require("./config/keys.js").mongoURI;
-const passport = require("passport");
+
 const bodyParser = require("body-parser");
 
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
+const passport = require("passport");
 
 // body-parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -23,7 +24,14 @@ mongoose
 app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
-
 app.use(passport.initialize());
 
-require("./config/passport.js")(passport);
+require("./config/passport")(passport);
+//testing connection
+app
+  .get("/", (req, res) => {
+    res.send("connection success");
+  })
+  .listen(port, () => {
+    console.log(`server is running on ${port}`);
+  });
